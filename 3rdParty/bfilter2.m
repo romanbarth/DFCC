e0a3ec70a51733e727a2be01910e697457af5d75
romanbarth <37847173+romanbarth@users.noreply.github.com>
2018-04-30 14:37:25 +0200

    update publication reference

--- 3rdParty/bfilter2.m
+++ 3rdParty/bfilter2.m
@@ -1,7 +1,9 @@
 % This section includes third-party license information for certain third-party 
 % functions included with the DFCC approach published in 
 % 
-% Shaban, H.A.; Barth, R.; Bystricky, K. title. Nucleic Acids Research, volume(issue):pages1-pages2, 2018
+%   Haitham A Shaban, Roman Barth, Kerstin Bystricky; Formation of correlated 
+%   chromatin domains at nanoscale dynamic resolution during transcription, 
+%   Nucleic Acids Research, gky269, https://doi.org/10.1093/nar/gky269
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
